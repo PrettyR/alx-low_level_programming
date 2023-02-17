0e7a88948262c26f2457715d3ca9513db5b4d463
PrettyR <prettyraradza@gmail.com>
2023-02-17 10:13:37 -0800

    Commit 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,49 +1,20 @@
 #include <stdio.h>
 /**
- * main - Program that prints combination of three digits
- *
- * Desciption: Program that prints all possible outcomes
- * of double digit numbers from the ANSI character set
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int digit_one = '0';
-	int digit_two, digit_three;
+	int i, j;
 
-
-	while (digit_one <='9')/* Parent loop for the first digit */
-	{
-		digit_two = '0';
-		while (digit_two <= '9')/* Child loop for second digit */
-		{
-			digit_three = '0';
-			while (digit_three <= '9')/* Child loop for second digit */
-			{
-				/*Prevent repeating combinations and same digits */
-				if (digit_one > digit_two || digit_two > digit_three ||
-				digit_one == digit_two || digit_two == digit_three)
-				{
-					digit_three++;
-					continue;
-				}
-				putchar(digit_one);
-				putchar(digit_two);
-				putchar(digit_three);
-
-				/* Prevents output if at the end of combination i.e 89 */
-				if (digit_one != '7' || digit_two != '8' || digit_three != '9')
-				{
-					putchar(',');
-					putchar(' ');
-				}
-				digit_three++;
-			}
-			digit_two++;
-		}
-		digit_one++;
-	}
+	for (j = 48; j < 58; j++)
+	for (i = j + 1; i < 58; i++)
+	putchar(j);
+	putchar(i);
+	if (j != 56 || i != 57)
+	putchar(44);
+	putchar(32);
 	putchar('\n');
 	return (0);
 }
