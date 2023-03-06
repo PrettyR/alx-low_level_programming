f158ae80f1609d731dfd04c94f5f4ebf61bb94f7
PrettyR <prettyraradza@gmail.com>
2023-03-06 12:54:38 -0800

    Commit 7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_chessboard - prints the chessboard
+ *
+ * @a: a pointer to an array of 8 strings, each containing 8 characters
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
